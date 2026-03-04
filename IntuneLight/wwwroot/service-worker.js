03bad2a2c81ef262cc101687208f728524995365
Nils Solberg <nils.solberg@outlook.com>
2026-03-04 10:34:43 +0100

    feat: remove PWA setup and improve logo responsiveness

--- IntuneLight/wwwroot/service-worker.js
+++
@@ -1,5 +0,0 @@
-﻿// Minimal service worker to enable installability only.
-
-self.addEventListener("install", () => self.skipWaiting());
-self.addEventListener("activate", (event) => event.waitUntil(self.clients.claim()));
-self.addEventListener("fetch", () => { /* no-op */ });
